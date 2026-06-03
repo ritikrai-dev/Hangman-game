64c981c7159b20fee124238065fc5c048abfec43
Ritik Rai <cs.ritikrai@gmail.com>
2026-06-03 22:28:57 +0530

    ui

---
+++ src/Start.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Start() {
+  return (
+    <>
+    <header className='header'>
+        <img src='image.png' alt='Logo' className='header-logo'/>
+        <h1 className='header-title'>Hangman Challenge</h1>
+    </header>
+    <iv>hiii</iv>
+    <aside className='model'><img src='' alt='hangman-model'/></aside>
+    <main className='main-container'>
+      <div className='button-container'>
+        <button></button>
+        <h2>Play</h2>
+      </div>
+    </main>
+    </>
+  )
+}
+
